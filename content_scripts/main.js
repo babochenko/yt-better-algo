e1b5cc5bd53edb2e30f19c8dcd35d8ab78fd8aed
denisbabochenko <babochenko.denis2@gmail.com>
2024-07-14 17:57:01 +0100

    bring everything into a content script, for testability

--- content_scripts/main.js
+++ content_scripts/main.js
@@ -1,3 +1,232 @@
+const CATCH_ALL = [];
+
+function queryScores(videos) {
+  return "- " + videos.join("\n- ");
+}
+
+function buildQuery(videos) {
+  return `I have a list of videos:
+  
+  ${queryScores(videos)}
+  
+  For each video, provide a score between 0 and 1, where 0 means that this video is` +
+  ` not helpful and distracting, and 1 means that this video is useful for my personal` +
+  ` growth. Respond with just the list of numbers, comma-separated, without spaces, ` +
+  ` prefixes, or any other delimiters`;
+}
+
+function parseScores(videos, resp) {
+  const scores = resp.split(",").map(Number);
+  const pairs = [];
+
+  for (let i = 0; i < videos.length; i++) {
+    pairs.push({title: videos[i], score: scores[i]});
+  }
+
+  return pairs;
+}
+
+async function scoreVideos(apiKey, videos) {
+  const endpoint = "https://api.openai.com/v1/chat/completions";
+
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${apiKey}`,
+  };
+  const systemQuery = "You are a helpful assistant.";
+  const userQuery = buildQuery(videos);
+  console.log(userQuery)
+
+  const body = JSON.stringify({
+    model: "gpt-4o",
+    messages: [
+      { role: "system", content: systemQuery },
+      { role: "user", content: userQuery },
+    ],
+  });
+
+  const response = await fetch(endpoint, {
+    method: "POST",
+    headers: headers,
+    body: body,
+  });
+  const data = await response.json();
+
+  if ("error" in data) {
+    console.log("rate limit exceeded");
+    return CATCH_ALL;
+  }
+
+  try {
+    const content = data.choices[0].message.content;
+    return parseScores(videos, content);
+  } catch (e) {
+    console.error(e);
+    return CATCH_ALL;
+  }
+}
+
+
+// ----- SW
+
+const MAX_VIDEO_THRESHOLD = 10;
+const GPT_BUFFER_SIZE = 15;
+
+class Buffer {
+  constructor(size) {
+    this.size = size;
+    this.buffer = [];
+  }
+
+  append(element) {
+    this.buffer.push(element);
+    if (this.buffer.length === this.size) {
+      return this.getNextBatch()
+    }
+  }
+
+  getNextBatch() {
+    if (this.buffer.length > 0) {
+      const buf = [...this.buffer];
+      console.log(`>>> ${buf}`)
+      this.clear()
+      return buf;
+    }
+  }
+
+  clear() {
+    this.buffer = []
+  }
+}
+
+let countDisplayed = 0;
+let countRemoved = 0;
+const buffer = new Buffer(GPT_BUFFER_SIZE);
+
+const stubScoreVideos = async (_, videos) => {
+  console.log('...', JSON.stringify(videos))
+  return new Promise(r => setTimeout(r, 1000)).then(_ => 
+    videos.map(v => ({title: v, score: Math.random()})))
+}
+
+const doScoreVideo = async (request) => {
+  if (chrome.runtime.scoreCount > MAX_VIDEO_THRESHOLD) {
+    console.log('Score response limit exceeded, stopping listener.');
+    return [];
+  }
+
+  const {
+    extensionEnabled: isEnabled,
+    openaiApiKey: apiKey,
+  } = await chrome.storage.sync.get(['extensionEnabled', 'openaiApiKey'])
+
+  const title = request.title;
+  if (!isEnabled) {
+    console.log('>> extension is disabled');
+    return [{ title, score: 1 }]
+  } else if (!apiKey) {
+    console.log('>> no openai api key');
+    return [{ title, score: 1 }]
+  }
+
+  const batch = buffer.append(title);
+  if (batch) {
+    return await scoreVideos(apiKey, batch)
+    // return await stubScoreVideos(apiKey, batch)
+  } else {
+    return []
+  }
+}
+
+const doScoreVideoTimeout = async () => {
+  if (chrome.runtime.scoreCount > MAX_VIDEO_THRESHOLD) {
+    console.log('Score response limit exceeded, stopping listener.');
+    return [];
+  }
+
+  const {
+    extensionEnabled: isEnabled,
+    openaiApiKey: apiKey,
+  } = await chrome.storage.sync.get(['extensionEnabled', 'openaiApiKey'])
+
+  if (!isEnabled) {
+    console.log('>> extension is disabled');
+    return []
+  } else if (!apiKey) {
+    console.log('>> no openai api key');
+    return []
+  }
+
+  console.log(JSON.stringify(buffer))
+  const batch = buffer.getNextBatch();
+  if (batch) {
+    return await scoreVideos(apiKey, batch)
+    // return await stubScoreVideos(apiKey, batch)
+  } else {
+    return []
+  }
+}
+
+const onScores = (scores, sendResponse) => {
+  const shouldDisplay = Object.groupBy(scores, ({score}) => score > 0.5)
+  countDisplayed += (shouldDisplay[true] || []).length
+  countRemoved += (shouldDisplay[false] || []).length
+
+  sendResponse({
+    action: 'onScore',
+    scores: scores,
+  });
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+let finalizer = null
+
+const onScoreVideoSW = async (msg, sendResponse) => {
+  if (countDisplayed >= 20 || countRemoved >= 50) {
+    console.log('hit a threshold on scored video count - not scoring any more videos')
+    sendResponse({
+      action: 'onStopLoading',
+    });
+    return;
+  }
+
+  if (!finalizer) {
+    console.log(Date())
+    finalizer = sleep(2000)
+      .then(() => doScoreVideoTimeout())
+      .then(s => {
+        console.log(Date())
+        console.log(JSON.stringify(s))
+        onScores(s, sendResponse)
+      })
+  }
+
+  const scores = await doScoreVideo(msg)
+  onScores(scores, sendResponse)
+}
+
+// what is this code supposed to do?
+// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//   if (changeInfo.status === 'complete') {
+//     countDisplayed = 0;
+//     countRemoved = 0;
+//     buffer.clear()
+//   }
+// });
+
+// awaits from the message from FE
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  if (msg.action === 'getScoreVideo') {
+    onScoreVideoSW(msg, sendResponse)
+    return true
+  }
+})
+
+// ----- MAIN
+
 const logSeen = '[filter_seen]'
 const logRm = '[filter_rm]'
 const logNew = '[vid]'
@@ -168,8 +397,7 @@ const observeVideos = (videos) => {
 
       const title = video.querySelector("#video-title");
       if (title) {
-        chrome.runtime.sendMessage({
-          action: "getScoreVideo",
+        onScoreVideoSW({
           title: title.innerText,
         }, resp => {
           if (resp.action === 'onScore') {
